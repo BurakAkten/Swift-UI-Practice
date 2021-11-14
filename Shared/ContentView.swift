f7ae30edf2cce78a6f86f27cf0defe02fe0c5e4d
Burak Akten <burak_akten@hotmail.com>
2021-11-14 14:36:37 +0300

    HStack, VStack, ZStack and other simple view pracrices.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    fileprivate func getZStack() -> some View {
+        return ZStack(){
+            Rectangle().foregroundColor(.indigo)
+            Circle().foregroundColor(.yellow).padding()
+            Text("Hello Body")
+                .font(.title)
+                .foregroundColor(.blue)
+                .multilineTextAlignment(.center)
+                .padding(12)
+        }.padding()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Circle().padding(.all, 12.0).foregroundColor(Color.purple)
+            Spacer(minLength: 12)
+            HStack(){
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.brown).padding()
+                getZStack()
+            }
+        }
     }
 }
 
