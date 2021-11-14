5a3614d4495e6dc3a302e19f5d71cf793382ca36
Burak Akten <burak_akten@hotmail.com>
2021-11-14 16:05:17 +0300

    Grouping, marking and button view added and experienced.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,27 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    fileprivate func getZStack() -> some View {
-        return ZStack(){
-            Rectangle().foregroundColor(.indigo)
-            Circle().foregroundColor(.yellow).padding()
-            Text("Hello Body")
-                .font(.title)
-                .foregroundColor(.blue)
-                .multilineTextAlignment(.center)
-                .padding(12)
-        }.padding()
-    }
-    
-    var body: some View {
-        VStack {
+    //MARK: Geometric Shapes
+    fileprivate func geometricShapes() -> some View {
+        return VStack {
             Circle().padding(.all, 12.0).foregroundColor(Color.purple)
             Spacer(minLength: 12)
             HStack(){
                 RoundedRectangle(cornerRadius: 12).foregroundColor(.brown).padding()
-                getZStack()
+                ZStack(){
+                    Rectangle().foregroundColor(.indigo)
+                    Circle().foregroundColor(.yellow).padding()
+                    Text("Hello Body")
+                        .font(.title)
+                        .foregroundColor(.blue)
+                        .multilineTextAlignment(.center)
+                        .padding(12)
+                }.padding()
             }
         }
+    }
+    //MARK: Clicable Group View
+    fileprivate func clicableGroupView() -> some View{
+        return VStack(alignment: .center, spacing: 12) {
+            Spacer()
+            Group(){
+                Text("This")
+                Text("is")
+                Text("Sparta!!")
+            }.font(.headline).foregroundColor(.red).shadow(radius: 4)
+            Spacer()
+            Button(action: onClick) {
+                HStack(){
+                    Image(systemName: "person").foregroundColor(.white)
+                    Text("Clickable")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+            }
+            .buttonStyle(.bordered)
+            .background(Color("button_bg"))
+            .buttonBorderShape(.roundedRectangle)
+            .cornerRadius(12)
+        }
+    }
+    
+    var body: some View {
+        let showShapes = false;
+        Group(){
+            if showShapes {
+                geometricShapes()
+            } else{
+                clicableGroupView()
+            }
+        }
+    }
+    
+    func onClick(){
+        print("On click");
     }
 }
 
