c03433bde1ff7f6740fa6d2a7e19d3edcf17080c
Burak Akten <burak.akten@profe.com.tr>
2021-11-14 13:48:44 +0300

    Initial Commit

---
+++ Shared/Swift_UI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_PracticeApp.swift
+//  Shared
+//
+//  Created by Burak Akten on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
